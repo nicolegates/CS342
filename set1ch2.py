096b2e28f4bdf6975551fa65de897d85c1b117cd
nicolegates <ngates@wellesley.edu>
2018-10-17 15:19:49 -0400

    Finished challenge 1 and 2 on cryptopals for set 1

---
+++ set1ch2.py
@@ -0,0 +1,34 @@
+import set1ch1
+
+x = "1c0111001f010100061a024b53535009181c"
+y = "686974207468652062756c6c277320657965"
+
+# create byte arrays
+b1 = bytearray()
+b2 = bytearray()
+
+# convert strings into byte arrays
+b1.extend(x.encode())
+b2.extend(y.encode())
+
+# XOR each byte against each other
+for i in range(len(b1)):
+    b1[i] = b1[i] ^ b2[i]
+
+#print result
+for i in range(len(b1)):
+    print(b1[i])
+
+# the XOR function: takes two values in hex, XORs them, and returns
+# a byte array containing the XORed values
+def XOR(x, y):
+    b1 = bytearray()
+    b2 = bytearray()
+    
+    b1.extend(x.encode())
+    b2.extend(y.encode())
+
+    for i in range(len(b1)):
+        b1[i] = b1[i] ^ b2[i]
+
+    return b1
